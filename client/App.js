e59ba55b9b991168ae498e83398d6936bd5b110c
angga <angga220801@gmail.com>
2020-11-14 14:57:49 +0700

    done without redux

--- client/App.js
+++ client/App.js
@@ -6,23 +6,23 @@ import store from './store'
 import { NavigationContainer, useLinkProps } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Landing from './screens/Landing'
-import Login from './screens/Landing'
+import Login from './screens/Login'
 import Register from './screens/Register'
 
 
 
-const Stack = createStackNavigator();
 
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Landing" component={Landing} 
-        options={{title: 'Landing'}}
-        />
         <Stack.Screen name="Login" component={Login} 
         options={{title: 'Login'}}
+        />
+        <Stack.Screen name="Landing" component={Landing} 
+        options={{title: 'Landing'}}
         />
         <Stack.Screen name="Register" component={Register} 
         options={{title: 'Register'}}
--- client/screens/Register.jsx
+++ client/screens/Register.jsx
@@ -1,12 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import axios from 'axios'
 
-export default function Register() {
+
+export default function Register({navigation}) {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [address, setAddress] = useState('')
+
+
+  const handleRegister = () => {
+    axios({
+      url: 'http://192.168.1.5:3000/users/register',
+      method: 'POST',
+      data: {
+        name,email,password,address
+      }
+    })
+    .then((res) => {
+      console.log(res, '<<<<<<<RESPONYA');
+    })
+    .catch((err) => {
+      console.log(err, '<<<<<<<<<ERRRRRROOORRRRRR');
+    })
+  }
   return (
     <View style={styles.container}>
       <Text>Register</Text>
-      <StatusBar style="auto" />
+      
+      <Text>Username: {name} </Text>
+      <Text>email: {email}</Text>
+      <Text>password: {password}</Text>
+      <Text>Address: {address} </Text>
+
+
+      <TextInput
+      style={styles.formInput}
+      autoCompleteType="off"
+      onChangeText={(text) => setName(text)}
+      />
+      <TextInput
+      style={styles.formInput}
+      autoCompleteType="off"
+      onChangeText={(text) => setEmail(text)}
+      />
+       <TextInput
+      style={styles.formInput}
+      autoCompleteType="off"
+      onChangeText={(text) => setPassword(text)}
+      secureTextEntry={true}
+      />
+      <TextInput
+      style={styles.formInput}
+      autoCompleteType="off"
+      onChangeText={(text) => setAddress(text)}
+      />
+
+      <Button
+      title="Register"
+      onPress={handleRegister}
+      />
+
+      <Text>Already had an account? 
+       <Text 
+       style={{color:'blue', textDecorationLine: 'underline'}}
+       accessibilityRole='button'
+       onPress={(e) => {
+         e.preventDefault()
+         navigation.replace('Login')
+          }
+        }
+      > Login</Text>
+
+     </Text>
     </View>
   );
 }
@@ -18,4 +86,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  formInput: {
+    width: 100,
+    borderWidth: 2
+  }
 });
