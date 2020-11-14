7d6c64ecc9d6c19a53c795c6994689843be7acc8
hutamafs <hutamafajarsaputra@gmail.com>
2020-11-14 20:41:12 +0700

    server fix pet-controller create

--- client/store/reducers/index.js
+++ client/store/reducers/index.js
@@ -1,11 +1,14 @@
 const initialState = {
-    access_token: ''
+    access_token: '',
+    pets:[]
 }
 
 export default function reducer (state = initialState, action) {
     switch (action.type) {
         case 'SET_TOKEN': 
-            return {...state, access_token: action.payload}
+            return {...state, access_token: action.payload};
+        case 'ADD_PET':
+            return {...state, pets:state.pets.concat(action.payload)};
         default:
             return state
     }
