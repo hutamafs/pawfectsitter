6ce4762ea76f689bb4f88874fd0b80728ef06523
hutamafs <hutamafajarsaputra@gmail.com>
2020-11-15 19:11:37 +0700

    add pet masi failed

--- client/App.js
+++ client/App.js
@@ -42,12 +42,12 @@ export default function App() {
           {/* <Stack.Screen name="PetList" component={PetList} 
             options={{title: 'PetList'}}
             /> */}
-            <Stack.Screen name="KeepersPage" component={KeepersPage}
+            {/* <Stack.Screen name="KeepersPage" component={KeepersPage}
            options={{title: 'Keepers'}}
            />
             <Stack.Screen name="Landing" component={Landing} 
             options={{title: 'Landing'}}
-            />
+            /> */}
             
                 <Stack.Screen name="AddPet" component={AddPet} 
           options={{title: 'AddPet'}}
