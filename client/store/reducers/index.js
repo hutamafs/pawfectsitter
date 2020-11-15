a190cb294340be0e660474368eda7c0f3aeaf462
angga <angga220801@gmail.com>
2020-11-15 11:20:28 +0700

    before pull

--- client/store/reducers/index.js
+++ client/store/reducers/index.js
@@ -2,11 +2,8 @@ const initialState = {
     keepers: [],
     access_token: '',
     pets:[],
-<<<<<<< HEAD
-    orders:[]
-=======
+    orders:[],
     loading: true
->>>>>>> 19d48e538b811cac522b4a94eb7be9f430c6b4cc
 }
 
 export default function reducer (state = initialState, action) {
@@ -17,13 +14,11 @@ export default function reducer (state = initialState, action) {
             return {...state, access_token: action.payload};
         case 'ADD_PET':
             return {...state, pets:state.pets.concat(action.payload)};
-<<<<<<< HEAD
         case 'SET_ORDERS':
             return {...state, orders: action.payload}
-=======
+
         case 'FETCH_PETS':
             return {...state, pets: action.payload, loading: false}
->>>>>>> 19d48e538b811cac522b4a94eb7be9f430c6b4cc
         default:
             return state
     }
